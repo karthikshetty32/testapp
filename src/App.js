0c73b9abc25333dc7d0a46852f4aa576c0f3b4e8
karthikshetty32 <58006183+karthikshetty32@users.noreply.github.com>
2021-12-15 09:57:42 +0530

    Small changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello KARTHIK SHETTY
+          Hello KARTHIK SHETTY AND MANJUNATHA 
         </p>
         <a
           className="App-link"
