4288aba817335e83835cb5a1f9e13e4fd384ac39
Karthik32 <karthiksshetty1996@gmail.com>
2022-01-05 18:07:21 +0530

    code fix

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import web from "../src/images/img1.jpg";
-import { NavLink } from "react-router-dom";
+//import { NavLink } from "react-router-dom";
 import Commom from "./Commom";
 
 const Home = () => {
