a4368d043efed71373e03aaf005b8d6afed22173
Karthik32 <karthiksshetty1996@gmail.com>
2022-01-05 17:48:03 +0530

    updated code

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import web from "../src/images/img2.jpg";
+import { NavLink } from "react-router-dom";
+import Commom from "./Commom";
+
+const About = () => {
+  return (
+    <>
+      <Commom
+        name="About Us:"
+        imgsrc={web}
+        visit="/contact"
+        btname="Contact Now"
+      />
+    </>
+  );
+};
+
+export default About;
