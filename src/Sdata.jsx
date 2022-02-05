710f78bcd29f36d5a43a91876f4691af46b53819
karthikshetty32 <58006183+karthikshetty32@users.noreply.github.com>
2022-02-05 10:59:37 +0530

    removed

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -2,8 +2,8 @@ import course1 from "../src/images/course1.jpeg";
 import course2 from "../src/images/course2.jpeg";
 import course3 from "../src/images/course3.jpeg";
 import course4 from "../src/images/course4.jpeg";
-import course5 from "../src/images/course5.jpeg";
-import course6 from "../src/images/course6.jpeg";
+//import course5 from "../src/images/course5.jpeg";
+//import course6 from "../src/images/course6.jpeg";
 
 const Sdata = [
   {
@@ -22,14 +22,7 @@ const Sdata = [
     imgsrc: course4,
     title: "AI & ML",
   },
-  {
-    imgsrc: course5,
-    title: "Digital Marketing",
-  },
-  {
-    imgsrc: course6,
-    title: "Big Data",
-  },
+ 
   
 ];
 
